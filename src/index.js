5c1217b823d53f65e3ea7db6c1861195d5298a45
Melania P Chagas <melaniapchagas@gmail.com>
2022-10-13 17:18:29 -0300

    Requisito 1 :rocket:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const app = express();
 app.use(bodyParser.json());
+const fs = require('fs').promises;
+const path = require('path');
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -11,6 +12,17 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const pathPalestrantesCadastrados = path.resolve(__dirname, 'talker.json');
+  const palestrantesCadastrados = JSON.parse(await fs
+    .readFile(pathPalestrantesCadastrados, 'utf-8'));
+
+  if (!palestrantesCadastrados) {
+    return [];
+  }
+  return res.status(HTTP_OK_STATUS).json(palestrantesCadastrados);
 });
 
 app.listen(PORT, () => {
