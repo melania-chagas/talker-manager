50225c277f7087051ba6e38b7449fbaa73484969
Melania P Chagas <melaniapchagas@gmail.com>
2022-10-18 20:43:17 -0300

    Requisito 7

--- src/routers/talker.router.js
+++ src/routers/talker.router.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const fs = require('fs').promises;
 const path = require('path');
 
+const PATH = '../talker.json';
+
 const validationTalker = require('../middlewares/validationTalker');
 
 const HTTP_OK_STATUS = 200;
 const NOT_FOUND = 404;
 
 router.get('/talker', async (req, res) => {
-  const pathPalestrantesCadastrados = path.resolve(__dirname, '../talker.json');
+  const pathPalestrantesCadastrados = path.resolve(__dirname, PATH);
   const palestrantesCadastrados = JSON.parse(await fs
     .readFile(pathPalestrantesCadastrados, 'utf-8'));
 
@@ -21,7 +23,7 @@ router.get('/talker', async (req, res) => {
 });
 
 router.get('/talker/:id', async (req, res) => {
-  const pathPalestrantesCadastrados = path.resolve(__dirname, '../talker.json');
+  const pathPalestrantesCadastrados = path.resolve(__dirname, PATH);
   const palestrantesCadastrados = JSON.parse(await fs
     .readFile(pathPalestrantesCadastrados, 'utf-8'));
 
@@ -40,7 +42,7 @@ router.get('/talker/:id', async (req, res) => {
 router.post('/talker', validationTalker, async (req, res) => {
   const talker = req.body;
 
-  const pathPalestrantesCadastrados = path.resolve(__dirname, '../talker.json');
+  const pathPalestrantesCadastrados = path.resolve(__dirname, PATH);
   const palestrantesCadastrados = JSON.parse(await fs
     .readFile(pathPalestrantesCadastrados, 'utf-8'));
   talker.id = palestrantesCadastrados.length + 1;
@@ -53,7 +55,7 @@ router.post('/talker', validationTalker, async (req, res) => {
 router.put('/talker/:id', validationTalker, async (req, res) => {
   const talker = req.body;
 
-  const pathPalestrantesCadastrados = path.resolve(__dirname, '../talker.json');
+  const pathPalestrantesCadastrados = path.resolve(__dirname, PATH);
   const palestrantesCadastrados = JSON.parse(await fs
     .readFile(pathPalestrantesCadastrados, 'utf-8'));
   const id = Number(req.params.id);
@@ -65,6 +67,26 @@ router.put('/talker/:id', validationTalker, async (req, res) => {
   await fs.writeFile(pathPalestrantesCadastrados, JSON
     .stringify(palestrantesCadastrados, null, 2), 'utf-8');
   return res.status(200).json(talker);
+});
+
+router.delete('/talker/:id', async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).send({ message: 'Token não encontrado' }); 
+  }
+  if (authorization.length !== 16) { 
+    return res.status(401).send({ message: 'Token inválido' });
+  }
+  const { id } = req.params;
+  const pathPalestrantesCadastrados = path.resolve(__dirname, PATH);
+  const palestrantesCadastrados = JSON.parse(await fs
+    .readFile(pathPalestrantesCadastrados, 'utf-8'));
+  const palestrante = palestrantesCadastrados.find((t) => t.id === id);
+  const index = palestrantesCadastrados.indexOf(palestrante);
+  palestrantesCadastrados.splice(index, 1);
+  await fs.writeFile(pathPalestrantesCadastrados, JSON
+    .stringify(palestrantesCadastrados, null, 2), 'utf-8');
+  return res.sendStatus(204);
 });
 
 module.exports = router;
